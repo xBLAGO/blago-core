6412f567b16e0af200fefc0b383aa12a7b6cd8f1
Roman Maloev <sdelayblago@gmail.com>
2025-03-26 13:25:30 +0300

    +

--- tests/NeedNFT_test.js
+++ tests/NeedNFT_test.js
@@ -1,1 +1,12 @@
-// Test NeedNFT contract functionality
+const { TonClient } = require("@tonclient/core");
+const NeedNFT = require("../build/NeedNFT");
+
+const client = new TonClient({ network: { endpoints: ["https://testnet.toncenter.com/api/v2/jsonRPC"] } });
+
+async function testCreateNeed() {
+  const contract = await client.load(NeedNFT, "0:...");
+  await contract.create_need({ description: "MacBook PRO, 2020, 80000 RUB", cost: 80000 });
+  console.log("Need created");
+}
+
+testCreateNeed();
